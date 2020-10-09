4cd64591e9c4a35add0acc357c09f64a0cce02fe
Dhadhazi <dhadhazi@gmail.com>
2020-10-09 07:59:13 +0200

    Dark mode added

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,34 @@ import { Route, Switch } from "react-router-dom";
 
 import "react-dropdown/style.css";
 import "./styles/App.scss";
+
 import { FlagList } from "./components/FlagList";
 import { Detail } from "./components/Detail";
 
 function App() {
-  const [darkmode, setDarkmode] = useState<boolean>(true);
+  const [darkmode, setDarkmode] = useState<boolean>(false);
+
+  if (darkmode)
+  {
+    document.documentElement.style.setProperty('--background', 'hsl(209, 23%, 22%)');
+    document.documentElement.style.setProperty('--elem', ' hsl(207, 26%, 17%)');
+    document.documentElement.style.setProperty('--text', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--input', 'hsl(207, 26%, 17%)');
+} 
+  
+  else {
+    document.documentElement.style.setProperty('--background', 'hsl(0, 0%, 98%)');
+    document.documentElement.style.setProperty('--elem', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--text', 'hsl(200, 15%, 8%)');
+    document.documentElement.style.setProperty('--input', ' hsl(0, 0%, 52%)'); 
+  }
 
   return (
     <div className="maindiv">
       <div className="topmenu">
         <div className="positioner">
           <header>Where in the world?</header>
-          <div className="mode">
+          <div className="mode" onClick={()=>setDarkmode((prevState)=>!prevState)}>
             <i className="far fa-moon"></i> Dark mode
           </div>
         </div>
